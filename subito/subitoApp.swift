1a04695520085234414d0a74262cf8ece213ab0b
BrGEDev <subgerencia-sistemas@asicofi.com>
2024-09-09 11:20:03 -0600

    Initial Commit

---
+++ subito/subitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  subitoApp.swift
+//  subito
+//
+//  Created by Brandon Guerra Espinoza  on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct subitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
